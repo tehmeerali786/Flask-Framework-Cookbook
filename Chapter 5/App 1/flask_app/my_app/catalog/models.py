5fb9ec3b21dc6c3afb17f90888b4999d61ab528d
tehmeerali786 <freelanceali786@gmail.com>
2023-12-27 17:56:37 -0800

    Chapter 5 First

---
+++ Chapter 5/App 1/flask_app/my_app/catalog/models.py
@@ -0,0 +1,53 @@
+import datetime
+from decimal import Decimal
+from my_app import db 
+from wtforms import StringField, DecimalField, SelectField 
+from wtforms.validators import InputRequired, NumberRange, ValidationError
+from flask_wtf import FlaskForm
+
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref=db.backref('products', lazy='dynamic'))
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price 
+        self.category = category
+        
+    def __repr__(self):
+        return '<Product %d>' % self.id
+
+# class Product(db.Document):
+#     created_at = db.DateTimeField(
+#         default=datetime.datetime.now, required=True
+#     )
+#     key = db.StringField(max_length=100, required=True)
+#     name = db.StringField(max_length=100, required=True)
+#     price = db.DecimalField()
+#     def __repr__(self):
+#         return '<Product %r>' % self.id
+        
+    
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    def __init__(self, name):
+        self.name = name 
+    def __repr__(self):
+        return '<Category %d>' % self.id
+    
+
+class NameForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    
+class ProductForm(NameForm):
+    # name = StringField('Name', validators=[InputRequired()] )
+    price = DecimalField('Price', validators=[InputRequired(), NumberRange(min=Decimal('0.0'))])
+    category = SelectField('Category', validators=[InputRequired()] ,coerce=int)
+    
+class CategoryForm(NameForm):
+    pass
---
+++ Chapter 5/App 1/flask_app/my_app/catalog/views.py
@@ -0,0 +1,165 @@
+from decimal import Decimal
+import redis
+from functools import wraps
+from flask import request, jsonify, Blueprint, render_template, flash, redirect, url_for
+from my_app import db, app
+from my_app.catalog.models import Product, Category, ProductForm, CategoryForm
+# from my_app import redis
+from sqlalchemy.orm import join
+
+catalog = Blueprint('catalog', __name__)
+
+
+def template_or_json(template=None):
+    '''Return a dict from your view and this will either
+        pass it to a template or render json. Use like:
+        
+        @template_or_json('template.html')
+    '''
+    def decorated(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            ctx = f(*args, **kwargs)
+            if request.headers.get("X-Requested-Width") == "XMLHttpRequest" or not template:
+                return jsonify(ctx)
+            else:
+                return render_template(template, **ctx)
+        
+        return decorated_function
+    return decorated
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@catalog.route('/')
+@catalog.route('/home')
+@template_or_json('home.html')
+def home():
+    
+    products = Product.query.all()
+    
+    # if request.headers.get("X-Requested-With") ==\
+    #     "XMLHttpRequest":
+    #         products = Product.query.all()
+    #         return jsonify({
+    #             'count': len(products)
+    #         })
+    
+    return {'count': len(products)}
+
+@catalog.route('/product/<id>')
+def product(id):
+    # product = Product.objects.get_or_404(key=key)
+    product = Product.query.get_or_404(id)
+    product_key = 'product-%s' % product.id
+    # redis.set(product_key, product.name)
+    # redis.expire(product_key, 600)
+    return 'Product - %s, %s' % (product.name, product.price)
+
+@catalog.route('/products')
+@catalog.route('/products/<int:page>')
+def products(page=1):
+    products = Product.query.paginate(page=page, per_page=10)
+    # res = {}
+    # for product in products:
+    #     res [product.id] ={
+    #         'name' : product.name,
+    #         'price' :str(product.price),
+    #         'category': product.category.name,
+    #     }
+    # return jsonify(res)
+    return render_template('products.html', products=products)
+
+
+@catalog.route('/product-create', methods=['GET', 'POST'])
+def create_product():
+    
+    form = ProductForm(meta={'csrf': False})
+    categories =[(c.id, c.name) for c in Category.query.all()]
+    form.category.choices = categories
+    if form.validate_on_submit():
+        
+        name = form.name.data
+        price = form.price.data
+        category = Category.query.get_or_404( form.category.data )
+        
+        product = Product(name, price, category)
+        # product.save()
+        db.session.add(product)
+        db.session.commit()
+        flash('This product %s has been created' % name, 'success')
+        return redirect(url_for('catalog.product', id=product.id))
+    if form.errors:
+        flash(form.errors, 'danger')
+    return render_template('product-create.html', form=form)
+
+@catalog.route('/product-search')
+@catalog.route('/product-search/<int:page>')
+def product_search(page=1):
+    name = request.args.get('name')
+    price = request.args.get('price')
+    category = request.args.get('category')
+    products = Product.query
+    if name:
+        products = products.filter(Product.name.like('%' + name + '%'))
+    if price:
+        products = products.filter(Product.price == price)
+    if category:
+        products = products.select_from(join(Product, Category)).filter(
+            Category.name.like('%' + category + '%')
+        )
+        
+    return render_template(
+        
+        'products.html', products=products.paginate(page=page, per_page=10)
+                           
+                           )
+
+
+@catalog.route('/create-category', methods=['GET', 'POST'])
+def create_category():
+    form = CategoryForm(meta={'csrf':False})
+    if form.validate_on_submit():
+        name = form.name.data
+        category = Category(name)
+        db.session.add(category)
+        db.session.commit()
+        flash(
+            'The category %s has been created successfully.' % name, 'success'
+        )
+        return redirect( url_for('catelog.category', id=category.id) )
+    
+    if form.errors:
+        flash(form.errors, 'danger')
+    return render_template('category-create.html', category=category)
+
+@catalog.route('/category/<id>')
+def category(id):
+    category = Category.query.get_or_404(id)
+    return render_template('category.html', category=category)
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    # res = {}
+    # for category in categories:
+    #     res[category.id] = {
+    #         'name' : category.name
+    #     }
+    #     for product in category.products:
+    #         res[category.id]['products'] = {
+                
+    #             'id' : product.id,
+    #             'name' : product.name, 
+    #             'price' : product.price
+    #         }
+            
+    # return jsonify(res)
+    return render_template('categories.html', categories=categories)
+
+@catalog.route('/recent-products')
+def recent_products():
+    keys_live = redis.keys('product-*')
+    products = [redis.get(k).decode('utf-8') for k in keys_live]
+    return jsonify({ 'products': products})
